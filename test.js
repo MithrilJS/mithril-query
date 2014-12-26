14e7889c385dbfc7a1b1115671dd25dad9b5b6cf
Stephan Hoyer <ste.hoyer@gmail.com>
2014-12-26 12:55:35 +0100

    Add test for event fireing

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
+'use strict';
+
 var test = require('tape').test;
 var m = require('mithril');
 var mq = require('./');
+
+function noop() {}
+
+var events = {
+  onclick: noop,
+  onfocus: noop
+};
 
 var tagEl = m('span');
 var concatClassEl = m('.onetwo');
@@ -10,7 +19,12 @@ var innerString = 'Inner String';
 var devilEl = m('.three', 'DEVIL');
 var idClassEl = m('#three.three');
 var arrayOfArrays = [[m('#arrayArray')]];
-var el = m('div', [tagEl, concatClassEl, classEl, innerString, idEl, devilEl, idClassEl, arrayOfArrays, undefined]);
+var eventEl = m('#eventEl', {
+  onclick: function(evt) { events.onclick(evt); },
+  onfocus: function(evt) { events.onfocus(evt); },
+});
+var el = m('div', [tagEl, concatClassEl, classEl, innerString, idEl,
+                   devilEl, idClassEl, arrayOfArrays, undefined, eventEl]);
 
 test('first', function(t) {
   t.equal(mq(el).first('span'), tagEl, 'select by tag should work');
@@ -21,11 +35,26 @@ test('first', function(t) {
   t.equal(mq(el).first('#two'), idEl, 'select by id should work');
   t.equal(mq(el).first('div#two'), idEl, 'select by tag/id should work');
   t.equal(mq(el).first('.three#three'), idClassEl, 'select by .class#id should work');
-  t.equal(mq(el).first(':contains(DEVIL)'), devilEl, 'select by :content should work');
-  t.equal(mq(el).first(':contains(Inner String)'), innerString, 'select by :content should work');
-  t.equal(mq(el).first('#arrayArray'), arrayOfArrays[0][0], 'select of array in array defined el should work');
+  t.equal(mq(el).first(':contains(DEVIL)'), 'DEVIL', 'select by :content should work');
+  t.equal(mq(el).first(':contains(Inner String)'), innerString,
+                       'select by :content should work');
+  t.equal(mq(el).first('#arrayArray'), arrayOfArrays[0][0],
+                       'select of array in array defined el should work');
   t.end();
 });
+
+test('events', function(t) {
+  t.plan(2);
+  events.onclick = function() {
+    t.ok(true, 'clickevent should be fired');
+  };
+  mq(el).click('#eventEl');
+  events.onfocus = function() {
+    t.ok(true, 'focusevent should be fired');
+  };
+  mq(el).focus('#eventEl');
+});
+
 test('contains', function(t) {
   t.ok(mq(el).contains('DEVIL'), 'contain should work');
   t.ok(mq(el).contains('Inner String'), 'contain should work');
